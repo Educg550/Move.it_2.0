015de66e41cb3bd018837c6b9e01e76fe6d8a31c
Eduardo Cruz Guedes <eduardo9cruz@gmail.com>
2021-02-25 13:13:51 -0300

    Update Countdown.tsx

--- moveit-next/src/components/Countdown.tsx
+++ moveit-next/src/components/Countdown.tsx
@@ -8,7 +8,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function Countdown() {
   const { startNewChallenge } = useContext(ChallengeContext)
 
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -28,7 +28,7 @@ export function Countdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
     //setHasFinished(true);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
   }
 
   useEffect(() => {
