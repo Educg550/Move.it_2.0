e81cb644c695619b5d408c4c2fe17c99ad690fc2
Eduardo Cruz Guedes <eduardo9cruz@gmail.com>
2021-02-25 09:41:54 -0300

    Aula 03 - Timer setado em 25 minutos

--- moveit-next/src/components/Countdown.tsx
+++ moveit-next/src/components/Countdown.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -24,7 +24,7 @@ export function Countdown() {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
   }
 
   useEffect(() => {
